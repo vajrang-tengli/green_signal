0e0f356f8e1356c4a75134d7d48abad97a5824bf
vajrang-tengli <127771520+vajrang-tengli@users.noreply.github.com>
2024-08-03 11:47:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 
 # Initialize the Firebase Admin SDK
-cred = credentials.Certificate("mini-1bdfc-firebase-adminsdk-crl4f-c2349c1071.json")
+cred = credentials.Certificate("https://github.com/vajrang-tengli/green_signal/blob/main/mini-1bdfc-firebase-adminsdk-crl4f-c2349c1071.json")
 firebase_admin.initialize_app(cred,  {"databaseURL": "https://mini-1bdfc-default-rtdb.firebaseio.com/"})
 
 
